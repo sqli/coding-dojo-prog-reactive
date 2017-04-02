8d9ffcb974fd5bd1da164270d20d82c9c52e11c3
Robin Coma Delperier <rcomadelperier@sqli.com>
2017-04-02 17:14:56 +0200

    empty solution

--- main.js
+++ main.js
@@ -3,20 +3,3 @@
  */
 
 const Observable = Rx.Observable;
-
-const myObservable = new Observable(observer => {
-
-    observer.next('a');
-    observer.next('b');
-    observer.next('c');
-    observer.next('d');
-    observer.error('This is an error');
-    observer.complete();
-
-});
-
-myObservable.subscribe(
-    item => console.info(item),
-    error => console.error(error),
-    () => console.log('This is the end')
-);
