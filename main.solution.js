f60d7d8ee15f2ce67babf8ad66ad8cbc91e5263e
Robin Coma Delperier <rcomadelperier@sqli.com>
2017-04-02 17:28:48 +0200

    TP2

--- main.solution.js
+++ main.solution.js
@@ -4,18 +4,28 @@
 
 const Observable = Rx.Observable;
 
-const myObservable = new Observable(observer => {
+const myObservable1 = new Observable(observer => {
 
-    observer.next('a');
-    observer.next('b');
-    observer.next('c');
-    observer.next('d');
-    observer.error('This is an error');
+    observer.next(20);
+    observer.next(40);
+    observer.next(60);
+    observer.next(80);
+    observer.next(100);
     observer.complete();
 
 });
 
-myObservable.subscribe(
+const myObservable2 = new Observable(observer => {
+
+    observer.next(1);
+    observer.next(1);
+    observer.complete();
+
+});
+
+const myMergedObservable = Observable.merge(myObservable1, myObservable2).filter(x => x > 50);
+
+myMergedObservable.subscribe(
     item => console.info(item),
     error => console.error(error),
     () => console.log('This is the end')
